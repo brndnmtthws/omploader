0a8d147743d696f78a7c0fdec21e448f3404cf6f
Brenden Matthews <brenden@rty.ca>
2008-09-07 01:34:05 +0000

    couple small fixes for rewrite scht
    git-svn-id: http://svn.omploader.org/trunk/omploader@308 f3e9fb10-4bd1-4be2-8a77-beb9a613051d

--- rewrite.py
+++ rewrite.py
@@ -11,19 +11,20 @@ f = open(sys.path[0] + '/rewrites.txt', 'r')
 rewrites = f.readlines()
 regex_s = '('
 comment = re.compile('^(.*)(#.*)$')
-strip_whitespace = re.compile('\S*')
+strip_whitespace = re.compile('\s*(\S*)')
 for r in rewrites:
 	match = comment.match(r)
 	if match and len(match.group(1)) > 0:
 		r = match.group(1)
 	elif match:
 		continue
-	match = strip_whitespace.match(r)
+	match = strip_whitespace.search(r)
 	if match and len(match.group()) > 0:
-		r = match.group()
-	regex_s += r.strip('\n') + '|'
+		r = match.group(1)
+		regex_s += r.strip('\n') + '|'
 
-regex_s = regex_s[:len(regex_s)-1]
+if len(regex_s) > 1:
+	regex_s = regex_s[:len(regex_s)-1]
 regex_s += ')'
 regex = re.compile(regex_s, re.IGNORECASE)
 
